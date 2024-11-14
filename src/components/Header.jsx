882d865698d8dea324e1b372db510c21852eefd8
Adonay <adonay.rssantos@gmail.com>
2024-11-14 20:56:51 -0300

    Componentização do Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,18 @@
 import "./Header.css";
-import logo from "../assets/favicon.ico";
 import { Link } from "react-router-dom";
 
-export default function Header() {
+export default function Header(props) {
   return (
-    <>
-      <header className="header">
-        <div className="logo">
-          <img src={logo} alt="" />
-        </div>
-        <div className="title">
-          <h1>Página Inicial</h1>
-        </div>
-        <div className="links-nav">
-          <Link to="/login">Entrar</Link>
-        </div>
-      </header>
-    </>
+    <header className="header">
+      <div className="logo">
+        <img src={props.item.logo} alt="Logo" />
+      </div>
+      <div className="title">
+        <h1>{props.item.pag}</h1>
+      </div>
+      <div className="links-nav">
+        <Link to={props.item.navegateheader}>{props.item.nome}</Link>
+      </div>
+    </header>
   );
 }
